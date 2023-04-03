780daae16dcd37e96dad0237ae2c0a6160da0214
Denis Mihailapov <d.mihailapov@expasoft.tech>
2023-04-03 22:55:50 +0700

    jacobian gradient for Softmax

--- nn/softmax.py
+++ nn/softmax.py
@@ -1,5 +1,7 @@
 from abc import ABC
 
+import numpy as np
+
 from nn import IFunc
 from nn.utils.functions import softmax
 
@@ -13,5 +15,11 @@ class Softmax(IFunc, ABC):
         return self.probs
 
     def backward(self, d_out):
-        grad = self.forward(self.probs) * (1 - self.forward(self.probs))
-        return grad * d_out
+        """Calculate the jacobian of the Softmax function for the given set of inputs."""
+        sm_d_out = d_out.copy()
+        eye = np.eye(d_out.shape[1])
+        for prob, d_out_, batch in zip(self.probs, d_out, range(d_out.shape[0])):
+            grad = prob[:, np.newaxis] * (eye - prob[np.newaxis, :])
+            sm_d_out[batch] = grad.dot(d_out_)
+
+        return sm_d_out
